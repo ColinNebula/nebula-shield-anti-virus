35e8d6ccfb14d8a0250376ae4f45c45fb6088397
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2025-10-15 18:37:57 -0400

    Add application metadata and custom icon configuration

---
+++ build-resources/generate-icon.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create a simple 256x256 icon using Canvas or sharp if available
+// For now, we'll create a simple script to generate icon using electron-builder's icon conversion
+
+console.log('Icon generation script');
+console.log('Place a 512x512 PNG file named "icon.png" in the build-resources folder');
+console.log('electron-builder will automatically convert it to .ico, .icns, etc.');
+
+// This is a placeholder - you should add a proper 512x512 PNG icon
+// You can use online tools like:
+// - https://www.photopea.com/ (free Photoshop alternative)
+// - https://www.canva.com/ (easy icon creator)
+// - Or use the SVG file with Inkscape to export as PNG
